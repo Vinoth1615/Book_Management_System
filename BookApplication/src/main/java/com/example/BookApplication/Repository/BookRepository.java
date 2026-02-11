8f761ce8e756103af486d487a4c7e3ed6c7b1caf
Vinoth1615 <vinothdeva05@gmail.com>
2026-02-11 18:52:33 +0530

    Initial commit - Spring Boot Book Management CRUD API with H2 database

---
+++ BookApplication/src/main/java/com/example/BookApplication/Repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.example.BookApplication.Repository;
+
+import com.example.BookApplication.Entity.Book;
+import org.aspectj.apache.bcel.util.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Integer> {
+    public Book findBookByTitle(String title);
+}
---
+++ BookApplication/src/main/java/com/example/BookApplication/Service/BookService.java
@@ -0,0 +1,29 @@
+package com.example.BookApplication.Service;
+
+
+import com.example.BookApplication.Entity.Book;
+import com.example.BookApplication.Repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+    @Autowired
+    private BookRepository bookRepository;
+
+    public Book addBook(Book book) {
+       return bookRepository.save(book);
+    }
+
+    public Book getBookByName(String name){
+        return bookRepository.findBookByTitle(name);
+    }
+
+    public Book updateBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public void deleteBook(Integer id) {
+        bookRepository.deleteById(id);
+    }
+}
